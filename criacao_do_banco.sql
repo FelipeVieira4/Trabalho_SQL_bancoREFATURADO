bcb059ca23480db3a0326d8e4cccb2e929e7149b
FelipeVieira4 <felipevieira58238@gmail.com>
2023-06-10 21:35:11 -0300

    update: atualizado historia e as inserçoes

--- criacao_do_banco.sql
+++ criacao_do_banco.sql
@@ -4,38 +4,39 @@
 # Criação da tablea "localização"
 create table localizacao(
 	id int primary key auto_increment,
-    rua varchar(255) not null,
-    bairro varchar(255) not null
+	rua varchar(255) not null,
+	bairro varchar(255) not null
 );
 
 # Criação da tablea "crimes"
 create table crime(
 	id int primary key auto_increment,
-    data date not null,
-    id_local int not null,foreign key(id_local) references localizacao(id)
+  data date not null,
+  descricao varchar(255),
+	id_local int not null,foreign key(id_local) references localizacao(id)
 );
 
 #Criação da tebela "vitima"
 create table vitima(
 	id int primary key auto_increment,
-    nome varchar(255) not null,
-    id_crime int,foreign key(id_crime) references crime(id)
+  nome varchar(255) not null,
+  id_crime int,foreign key(id_crime) references crime(id)
 );
 
 #Criação da tebela "suspeito"
 create table suspeito(
 	id int primary key auto_increment,
-    nome varchar(255) not null,
-    telefone varchar(11),
-    fixaCriminal varchar(255),
-    idade int not  null
+  nome varchar(255) not null,
+  telefone varchar(11),
+  fixaCriminal varchar(255),
+  idade int not  null
 );
 
 
 #Criação da relação crime e suspeito
 create table crime_suspeito(
 	id int primary key auto_increment,
-  motivo varchar(255,
+  motivo varchar(255),
   id_crime int not null,foreign key(id_crime) references crime(id),
   id_suspeito int not null,foreign key(id_suspeito) references suspeito(id)
 );
@@ -43,6 +44,6 @@ create table crime_suspeito(
 #Criação da tabela "evidencia"
 create table evidencia(
 	id int primary key auto_increment,
-    item varchar(255) not null,
-    id_crime int not null,foreign key(id_crime) references crime(id)
+  item varchar(255) not null,
+  id_crime int not null,foreign key(id_crime) references crime(id)
 );
--- insercao_dados.sql
+++ insercao_dados.sql
@@ -1,72 +1,55 @@
 /*
 	Crime: 1
-
-    José Albes - Dono de joalheria e há boatos que ele era lider da ceita iluminati
-	Natanael L.  - Lenhador estava com muitas dividas
-    Renato Ramos - Porte de arma
-	Matheus Rodrigues - Jaqueta Preta ex-funcionario da joalheria do José Albes ja teve passagem pela policia por roubo
-    Kevin Motter - Fazia parte da ceita do José Albes
-
 */
 
 insert into localizacao(rua,bairro) values("R. João Carlos Stein"," Jaraguá Esquerdo");
-insert into crime (data,id_local) values("2023-03-15",1);
+insert into crime (data,descricao,id_local) values("2023-03-15","A vitima foi morta enquanto saia da joalheria depois de fechar a mesma, o suspeito usou arma de fogo para cometer o assasinato.",1);
 
 insert into vitima (nome,id_crime) values("José Alves",1);
 
 #inserção dos suspeitos do crime(1)
 insert into suspeito (nome,telefone,idade) values
-("Natanael A.","47666666666",76),
-("Renato Ramos","47963577418",45),
-("Matheus Rodrigues","47896654123",39),
-("Kevin Motter","47896654123",39);
+("Natanael A.","47666666666",39),
+("Renato Ramos","47963577418",28),
+("Kevin Motter","47896654123",57);
 
 
 #Relação entre crime(1) e os suspeitos
 insert into  crime_suspeito(id_crime, id_suspeito, motivo) values
 (1,1,"Filho mais velho do João Alves."), #Natanael A.
 (1,2,"Teve problemas juridicos com João Alves"), #Renato Ramos
-(1,3,"Segurança de João Alves e da Joalheria"), #Matheus Rodrigues
-(1,4,"Lider de uma ceitar triangular"); #Kevin Motter
+(1,3,"Lider de uma ceitar, e estava intereçado em comprar a joalheria do José Alve"); #Kevin Motter
 
-insert into evidencia(item,id_crime) values("Pistola 9MM",1);
+insert into evidencia(item,id_crime) values("cápisulas de uma arma de fogo",1);
 
 /*
 	Crime: 2
-
-	Matheus Rodrigues
-
 */
 
 insert into localizacao(rua,bairro) values("R. João Planincheck","Nova Brasília");
-insert into crime (data,id_local)values ("2023-03-30",2);
+insert into crime (data,descricao,id_local)values ("2023-03-30","A vitima foi atropelado carro e placa não identificados",2);
 
 insert into vitima (nome,id_crime)values ("Matheus Rodrigues",2);
 
 
-insert into  crime_suspeito(id_crime, id_suspeito, motivo) values (2,1,"Provavel vingança familiar");#Natanel A.
-insert into descricao(id_crime,motivo,id_suspeito) values (2,,1);
+insert into  crime_suspeito(id_crime, id_suspeito, motivo) values (2,1,"vingança familiar");#Natanel A.
 
-insert into evidencia(item,id_crime) values("Machado",2);
+insert into evidencia(item,id_crime) values("carta de Denuncia",2);
 
 /*
-
 	Crime - 3
-	Natanael A.
-
 */
 
-
 insert into localizacao(rua,bairro) values("R. Artur Gumz, 709"," Vila Nova");
-insert into crime (data,id_local) values("2023-4-15",3);
+insert into crime (data,descricao,id_local) values("2023-4-15","morto ao sair de uma casa de boliche",3);
 
 insert into vitima (nome,id_crime) values("Natanael A.",3);
 
 
 insert into suspeito (nome,telefone,idade) values("Arthur R.","666",26);
 insert into  crime_suspeito(id_crime,id_suspeito) values
-(3,2),
-(3,4),
-(3,5);
+(3,2),# Renato Ramos
+(3,3),# Kevin Motter
+(3,4);# Arthur R.
 
-insert into evidencia(item,id_crime) values("Carta Para Familia do Matheus Rodrigues",3);
+insert into evidencia(item,id_crime) values("cápisulas de uma arma de fogo",3);
