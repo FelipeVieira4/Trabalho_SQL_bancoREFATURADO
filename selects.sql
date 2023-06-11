9361b3d4453bb427a074fdd07cdf6e41a401ffce
FelipeVieira4 <felipevieira58238@gmail.com>
2023-06-11 17:29:09 -0300

    update: selects

--- selects.sql
+++ selects.sql
@@ -1,25 +1,16 @@
+#Nome|Idade|Suspeito Em Crime
+select suspeito.nome, suspeito.idade,count(crime_suspeito.id_crime) as "Participaçoes" from crime_suspeito
+inner join suspeito on suspeito.id =crime_suspeito.id_suspeito
+group by crime_suspeito.id_suspeito order by suspeito.idade;
 
-# Tabela com crimes,suspeitos,vitimas
-select crime.id, crime.tipoCrime as "Crime", suspeito.nome as "Suspeito", vitima.nome as "Vitima" from crime_suspeito
-inner join crime on crime.id = crime_suspeito.id_crime
-inner join suspeito on suspeito.id = crime_suspeito.id_suspeito
-inner join vitima on vitima.id_crime = crime.id; #where suspeito.idade < 30;
+#ID do Crime|Suspeito|Idade|Motivo
+crime_suspeito.id_crime as "CRIME", suspeito.nome, suspeito.idade, crime_suspeito.motivo from crime_suspeito
+inner join suspeito on suspeito.id =crime_suspeito.id_suspeito;
 
-select crime.tipoCrime, localizacao.bairro from crime
-inner join localizacao on crime.id_local=localizacao.id;
+#ID|Nome Vitima|Localização|Descrição
+select crime.id as "CRIME ID", vitima.nome as "VITIMA", localizacao.rua, localizacao.bairro, crime.descricao from crime
+inner join vitima on vitima.id_crime = crime.id
+inner join localizacao on localizacao.id = crime.id_local;
 
-select crime_suspeito.id,localizacao.bairro as "Locais", suspeito.nome as "Suspeito" from crime_suspeito
-inner join crime on crime.id = crime_suspeito.id_crime
-inner join suspeito on suspeito.id = crime_suspeito.id_suspeito
-inner join localizacao on localizacao.id = crime.id_local order by localizacao.bairro; #where localizacao.bairro = "rio molha";
-
-select suspeito.idade,suspeito.nome from suspeito order by suspeito.idade;
-
-select crime.id,crime.tipoCrime, evidencia.item as "Evidencia" from crime inner join evidencia on evidencia.id_crime = crime.id;
-
-
-select crime.id as "CRIME ID", crime.tipoCrime as "Tipo", suspeito.nome as "Suspeito", cumplice.nome as "Cumplice", localizacao.bairro as "Bairro" from crime_suspeito
-inner join crime on crime.id = crime_suspeito.id_crime
-inner join suspeito on suspeito.id = crime_suspeito.id_suspeito
-inner join cumplice on cumplice.id_crime = crime.id
-inner join localizacao on crime.id_local = localizacao.id;
+#Mostrar quantidades de suspeitos por crimes
+select crime_suspeito.id_crime as "CRIME",count(crime_suspeito.id_suspeito) as "Suspeitos" from crime_suspeito group by id_crime;
