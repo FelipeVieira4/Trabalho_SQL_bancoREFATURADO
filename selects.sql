768330fa28b6e1104f017b8fe345464f48cf351d
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-06-13 18:27:30 -0300

    UPDATE:add some clauses

--- selects.sql
+++ selects.sql
@@ -1,6 +1,6 @@
 #Nome|Idade|Suspeito Em Crime
 select suspeito.nome, suspeito.idade,count(crime_suspeito.id_crime) as "Participaçoes" from crime_suspeito
-inner join suspeito on suspeito.id =crime_suspeito.id_suspeito
+inner join suspeito on suspeito.id =crime_suspeito.id_suspeito where suspeito.idade > 30
 group by crime_suspeito.id_suspeito order by suspeito.idade;
 
 #ID do Crime|Suspeito|Idade|Motivo
@@ -11,7 +11,9 @@ inner join suspeito on suspeito.id =crime_suspeito.id_suspeito order by crime_su
 select crime.id as "CRIME ID", vitima.nome as "VITIMA", crime.data, evidencia.item, localizacao.rua, localizacao.bairro, crime.descricao from crime
 inner join vitima on vitima.id_crime = crime.id
 inner join localizacao on localizacao.id = crime.id_local
-inner join evidencia on evidencia.id_crime = crime.id;
+inner join evidencia on evidencia.id_crime = crime.id
+#where crime.id_local=1
+;
 
 #Mostrar quantidades de suspeitos por crimes
 select crime_suspeito.id_crime as "CRIME",count(crime_suspeito.id_suspeito) as "Suspeitos" from crime_suspeito group by id_crime;
